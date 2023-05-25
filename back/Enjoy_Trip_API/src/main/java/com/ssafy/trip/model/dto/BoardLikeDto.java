ea4fc5d16a89fbbbc3a6f6c7da7beb31c838c58b
김유정 <kyj8441@gmail.com>
2023-05-25 11:13:54 +0900

    Refactor Dtos

--- back/Enjoy_Trip_API/src/main/java/com/ssafy/trip/model/dto/BoardLikeDto.java
+++ back/Enjoy_Trip_API/src/main/java/com/ssafy/trip/model/dto/BoardLikeDto.java
@@ -1,25 +1,13 @@
 package com.ssafy.trip.model.dto;
 
-public class BoardLikeDto {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardLikeDto {
 	private int boardNo;
 	private int userNo;
-	public int getBoardNo() {
-		return boardNo;
-	}
-	public void setBoardNo(int boardNo) {
-		this.boardNo = boardNo;
-	}
-	public int getUserNo() {
-		return userNo;
-	}
-	public void setUserNo(int userNo) {
-		this.userNo = userNo;
-	}
-	@Override
-	public String toString() {
-		return "BoardLikeDto [boardNo=" + boardNo + ", userNo=" + userNo + "]";
-	}
-	
-	
 }
--- back/Enjoy_Trip_API/src/main/java/com/ssafy/trip/model/dto/ReviewDto.java
+++ back/Enjoy_Trip_API/src/main/java/com/ssafy/trip/model/dto/ReviewDto.java
@@ -1,5 +1,12 @@
 package com.ssafy.trip.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReviewDto {
 	private String reviewNo;
 	private String content;
@@ -7,47 +14,4 @@ public class ReviewDto {
 	private int attractionNo;
 	private int userNo;
 	private String nickname;
-	public String getReviewNo() {
-		return reviewNo;
-	}
-	public void setReviewNo(String reviewNo) {
-		this.reviewNo = reviewNo;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String getCreatetime() {
-		return createtime;
-	}
-	public void setCreatetime(String createtime) {
-		this.createtime = createtime;
-	}
-	public int getAttractionNo() {
-		return attractionNo;
-	}
-	public void setAttractionNo(int attractionNo) {
-		this.attractionNo = attractionNo;
-	}
-	public int getUserNo() {
-		return userNo;
-	}
-	public void setUserNo(int userNo) {
-		this.userNo = userNo;
-	}
-	public String getNickname() {
-		return nickname;
-	}
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
-	@Override
-	public String toString() {
-		return "ReviewDto [reviewNo=" + reviewNo + ", content=" + content + ", createtime=" + createtime
-				+ ", attractionNo=" + attractionNo + ", userNo=" + userNo + ", nickname=" + nickname + "]";
-	}
-	
-	
 }
